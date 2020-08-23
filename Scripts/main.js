55c83b9c63c4ed2fbb11610059aac5b02cccd885
Md Habibur Rahman <contact@devhabib.com>
2020-08-24 02:02:49 +0600

    working on service-cat, (working on footer, footer part 3 Popular tweets haven't completed

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+	"use strict";
+	jQuery(document).ready(function ($) {
+		new WOW().init();
+
+	});
+
+	// jQuery loading function starts          
+	jQuery(window).load(function () {
+	
+	});
+}(jQuery));
