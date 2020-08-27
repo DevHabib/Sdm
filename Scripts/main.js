647b00e490b79422e370aea9d6f0ef4d4d0f7a04
Md Habibur Rahman <contact@devhabib.com>
2020-08-28 03:31:58 +0600

    brand details completed

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,11 +2,15 @@
 	"use strict";
 	jQuery(document).ready(function ($) {
 		new WOW().init();
-
+		lightbox.option({
+			'resizeDuration': 200,
+			'wrapAround': true,
+			alwaysShowNavOnTouchDevices: true
+		})
 	});
 
 	// jQuery loading function starts          
-	jQuery(window).load(function () {
-	
-	});
+	// jQuery(window).load(function () {
+
+	// });
 }(jQuery));
