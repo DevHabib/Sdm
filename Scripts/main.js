12afdfa06c06afe8e3fe7658d31d9ab81f4fcc44
Md Habibur Rahman <contact@devhabib.com>
2020-10-04 06:51:23 +0600

    Contact Completed

--- Scripts/main.js
+++ Scripts/main.js
@@ -19,3 +19,29 @@
 
 
 var rellax = new Rellax('.rellax');
+
+var mySwiper = new Swiper('.swiper-container-contact', {
+	speed: 400,
+	spaceBetween: 15,
+	slidesPerView: 3,
+	loop: true,
+	autoplay: true,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+	},
+	breakpoints: {
+		280: {
+			slidesPerView: 1,
+			spaceBetween: 1
+		},
+		479: {
+			slidesPerView: 2,
+			spaceBetween: 15
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 15
+		}
+	}
+});
